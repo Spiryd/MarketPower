9a5b49e24724e56981c0f73f051de0bbed3c1c76
Spiryd <neumann.maks@gmail.com>
2023-01-14 20:31:34 +0100

    added deletion to watchlist and edited a name

--- backend/src/services/watch_list.rs
+++ backend/src/services/watch_list.rs
@@ -1,4 +1,4 @@
-use actix_web::{get, post, web::{Data, ReqData, Json}, Responder, HttpResponse};
+use actix_web::{get, post, web::{Data, ReqData, Json}, Responder, HttpResponse, delete};
 use serde::{Serialize, Deserialize};
 use sqlx::{self, FromRow};
 
@@ -12,7 +12,7 @@ struct WatchItem {
 
 
 #[derive(Debug, Serialize, Deserialize, FromRow)]
-struct CreateWatchItemBody {
+struct WatchItemBody {
     ticker: String,
 }
 
@@ -53,7 +53,7 @@ async fn fetch_watch_list(state: Data<AppState>, req_user: Option<ReqData<TokenC
 }
 
 #[post("/watchitem")]
-async fn post_watchitem(state: Data<AppState>, req_user: Option<ReqData<TokenClaims>>, body: Json<CreateWatchItemBody>) -> impl Responder {
+async fn post_watchitem(state: Data<AppState>, req_user: Option<ReqData<TokenClaims>>, body: Json<WatchItemBody>) -> impl Responder {
     match req_user {
         Some(user) => {
             let db = match user.security_lvl {
@@ -62,7 +62,7 @@ async fn post_watchitem(state: Data<AppState>, req_user: Option<ReqData<TokenCla
                 2 => &state.db_user,
                 _ => &state.db_auth
             };
-            let watchitem_body: CreateWatchItemBody = body.into_inner();
+            let watchitem_body: WatchItemBody = body.into_inner();
             match sqlx::query_as::<_, WatchItem>("INSERT INTO watch_list VALUES ($1, $2) RETURNING account_id, ticker")
             .bind(user.id)
             .bind(watchitem_body.ticker)
@@ -70,6 +70,31 @@ async fn post_watchitem(state: Data<AppState>, req_user: Option<ReqData<TokenCla
             .await
             {
                 Ok(watchlist) => HttpResponse::Ok().json(watchlist),
+                Err(error) => HttpResponse::InternalServerError().json(format!("{:?}", error)),
+            }
+        }
+        _ => HttpResponse::Unauthorized().json("Unable to verify identity"),
+    }
+}
+
+#[delete("/watchitem")]
+async fn delete_portfolio_item(state: Data<AppState>, req_user: Option<ReqData<TokenClaims>>, body: Json<WatchItemBody>) -> impl Responder {
+    match req_user {
+        Some(user) => {
+            let db = match user.security_lvl {
+                0 => &state.db_admin,
+                1 => &state.db_moderator,
+                2 => &state.db_user,
+                _ => &state.db_auth
+            };
+            let watch_item_body: WatchItemBody = body.into_inner();
+            match sqlx::query_as::<_, WatchItem>("DELETE FROM portfolio WHERE account_id = $1 AND ticker = $2 RETURNING * ")
+            .bind(user.id)
+            .bind(watch_item_body.ticker)
+            .fetch_all(db)
+            .await
+            {
+                Ok(portfolioitem) => HttpResponse::Ok().json(portfolioitem),
                 Err(error) => HttpResponse::InternalServerError().json(format!("{:?}", error)),
             }
         }
