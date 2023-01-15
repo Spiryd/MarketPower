82f468b85e16e15cd13c17360681427c7005d0f4
Spiryd <neumann.maks@gmail.com>
2023-01-15 20:35:13 +0100

    semi final before turn in

--- frontend/src/lib.rs
+++ frontend/src/lib.rs
@@ -1,8 +1,11 @@
 use yew::prelude::*;
+use yew_router::prelude::*;
 
 #[function_component(App)]
 pub fn app() -> Html {
     html! {
-        <div></div>
+        <div>
+            <h1>{"Hello Wrld"}</h1>
+        </div>
     }
 }
---
+++ frontend/src/router.rs
@@ -0,0 +1,13 @@
+use yew::prelude::*;
+use yew_router::prelude::*;
+
+#[derive(Clone, Debug, PartialEq)]
+pub enum Route {
+    
+}
+
+pub fn switch(route: Route) -> Html {
+    match route {
+        
+    }
+}
